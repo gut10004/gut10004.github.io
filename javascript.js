f73c8ea3bff35f9f6f020c13d9aeb42959105b81
gut10004 <missyvettegh@gmail.com>
2020-01-10 19:16:02 -0700

    fix line break

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
-var options= {
-    weekday:"long",
-    day:"numeric",
-    month:"long",
-    year:"numeric"
-}
-document.getElementById('outputDiv').innerHTML= new Date(document.lastModified).toLocaleDateString("en-US",options);
-document.getElementById('date').innerHTML= new Date(document.lastModified).getFullYear();
+let dayNames= ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday", "Sunday"];
+let months= ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+let d= new Date();
+let dayName= dayNames[d.getDay()];
+let monthName= months[d.getMonth()];
+let year= d.getFullYear();
+
+let fullDate= dayName + "," + monthName + "," + " " + d.getDate() + "," 
+
+document.getElementById("currentDate").textContent= fullDate;
