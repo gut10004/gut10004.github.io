4fd7b3526c3ffc7966cc155e54c9108404c1c490
gut10004 <missyvettegh@gmail.com>
2020-02-10 17:54:04 -0700

    banner js and its link in html

---
+++ lesson5/js/showBanner.js
@@ -0,0 +1,7 @@
+function showBanner() {
+    var date = new Date();
+    console.log(date.getDay())
+    if(date.getDay()==4) {
+        document.getElementById("banner").style.display= disp;
+    } 
+}
