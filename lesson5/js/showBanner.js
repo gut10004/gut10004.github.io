dbd8edce120a0379d11bb5e8712ce3ced1212cc6
gut10004 <missyvettegh@gmail.com>
2020-02-10 18:55:38 -0700

    banner js fix

--- lesson5/js/showBanner.js
+++ lesson5/js/showBanner.js
@@ -1,6 +1,6 @@
 function showBanner() {
-    var date = new Date();
-    console.log(date.getDay())
+    var date= new Date();
+    console.log(date.getDay());
     if(date.getDay()==4) {
         document.getElementById("banner").style.display= disp;
     } 
