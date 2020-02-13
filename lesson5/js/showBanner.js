754361985ba1ac0b85c4dfc42f8d69e90f8aca55
gut10004 <missyvettegh@gmail.com>
2020-02-12 19:29:07 -0700

    banner js

--- lesson5/js/showBanner.js
+++ lesson5/js/showBanner.js
@@ -2,5 +2,7 @@ var today = new Date();
 var day = today.getDay();
 
 if (day==5) {
-    document.getElementById("banner").style.display = disp;
-}
+    
+} 
+//display banner for user to see
+document.getElementById("banner").style.display = disp;
