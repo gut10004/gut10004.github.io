e04c41061f65796938f68f2df056f9ba2f30c377
gut10004 <gut10004@byui.edu>
2020-06-05 00:02:44 -0600

    lesson7

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,38 @@
+//get imgs with data-src
+
+const imagesToLoad= document.querySelectorAll('img[data-src]');
+
+const loadImages= (image)=>{
+    image.setAttribute('src', image.getAttribute('data-src'));
+    image.onload=()=>{
+        image.removeAttribute('data-src')
+    };
+
+};
+
+const imgOptions={
+    threshold:0
+};
+
+if('IntersectionObserver' in window) {
+    //is supported
+    const imgObserver= new IntersectionObserver((items)=>{
+        items.forEach((item)=>{
+            if(item.isIntersecting){
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    //load image if necessary
+    imagesToLoad.forEach((img)=>{
+        imgObserver.observe(img);
+    });
+}
+    
+else {// if not supported, then load all images
+    imagesToLoad.forEach((img)=>{
+        loadImages(img);
+    });
+}
