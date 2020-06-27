3bccac14fa5dfa4b1b32584268b3984be014a50f
gut10004 <gut10004@byui.edu>
2020-06-27 13:58:00 -0600

    s

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL= "https://api.openweather.org/data/2.5/weather?id=5604473&APPID=4221f5ae2af828109b1fbb5b1ed68248&units=imperial";
+const apiURL= "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=4221f5ae2af828109b1fbb5b1ed68248&units=imperial";
 
 fetch(apiURL)
   .then((response) => response.json())
@@ -15,10 +15,4 @@ fetch(apiURL)
 
     icon.setAttribute("src", imagesrc);
     icon.setAttribute("alt", desc);
-    s
-
-
-
-
-
 });
