261e993886c69c5040a2fc13229a4badaf77b9a5
gut10004 <gut10004@byui.edu>
2020-06-27 19:41:04 -0600

    s

--- lesson10b/js/forecast.js
+++ lesson10b/js/forecast.js
@@ -1,6 +1,4 @@
-const currentURL= 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=4221f5ae2af828109b1fbb5b1ed68248&units=imperial';
 const forecastURL= 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=4221f5ae2af828109b1fbb5b1ed68248&units=imperial';
-
 
 fetch(currentURL)
 .then((response) => response.json())
@@ -11,4 +9,4 @@ fetch(currentURL)
     const fivedayforecast= jsObject.list.filter(x=>x.dt_txt.includes("12:00:00"));
 
     console.log(fivedayforecast);
-    
+
