c25ce985414aea3a8b4b6197a61227b9a2620f6f
gut10004 <gut10004@byui.edu>
2020-07-28 20:47:43 -0600

    rentals

--- lesson12/js/rentalsjson.js
+++ lesson12/js/rentalsjson.js
@@ -1,12 +1,14 @@
-const requestURL = "https://gut10004.github.io/lesson12/js/rentals.json";
+const rentalsURL = "https://gut10004.github.io/lesson12/js/rentals.json";
 
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  
-  .then(function (jsonObject) {
-    console.table(jsonObject);  
+fetch(rentalsURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      console.log(jsObject);
+      document.getElementById("metro").textContent= jsObject.main[4];
+      document.getElementById("dio").textContent=jsObject.main[5];
+      document.getElementById("pcx").textContent=jsObject.main[6];
+      document.getElementById("atv").textContent=jsObject.main[7];
+      document.getElementById("fourdoor").textContent=jsObject.main[8];
+      document.getElementById("twodoor").textContent=jsObject.main[9];
       
-  	}
 });
