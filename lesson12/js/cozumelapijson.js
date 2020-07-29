bbc7aff83d855bec3d48de58ad6a934389d9d771
gut10004 <gut10004@byui.edu>
2020-07-28 23:46:22 -0600

    .

--- lesson12/js/cozumelapijson.js
+++ lesson12/js/cozumelapijson.js
@@ -7,31 +7,8 @@ fetch(liveURL)
       document.getElementById("currently").textContent= jsObject.weather[0].description;
       document.getElementById("temperature").textContent=jsObject.main.temp;
       document.getElementById("humidity").textContent=jsObject.main.humidity;
-      document.getElementById("windSpeed").textContent=jsObject.wind.speed;
 
-      let temperature = parseFloat(document.getElementById("temperature").textContent);
-      let windSpeed = parseFloat(document.getElementById("windSpeed").textContent);
-
-      let windChill = windChillCalc(temperature, windSpeed);
-
-      document.getElementById("windChillOutput").textContent = windChill;
-
-      function windChillCalc(temperature, windSpeed) {
-          if(windSpeed > 3 && temperature <= 50) {
-            let chill= 35.74 + 0.6215 * temperature - 35.75 * (Math.pow(windSpeed, 0.16)) + 0.4275 * temperature * Math.pow(windSpeed, 0.16);
-            chill= chill.toFixed(0) + "&deg;F";
-            document.getElementById("windChillOutput").innerHTML= chill;
-          }
-        
-          else{
-            return "N/A";
-          }
-      }
 });
-
-
-
-
 
 
 const forecastURL="https://api.openweathermap.org/data/2.5/forecast?id=3530103&units=imperial&APPID=f83583a6ff92c0dc1b81953ae42984d6&units=imperial";
